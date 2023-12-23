820ccb1f3e48c0c6f8761cdf586da0cbb186b87e
Muhammad Iqbal Makmur Al-Muniri <126215393+GalaAldebara@users.noreply.github.com>
2023-12-23 14:44:47 +0700

    Delete bootstrap directory

--- bootstrap/php/footer.php
+++
@@ -1,7 +0,0 @@
-<footer class="sticky-footer bg-white" style="border-bottom-left-radius: 20px; border-bottom-right-radius:20px;">
-    <div class="container my-auto">
-        <div class="copyright text-center my-auto">
-            <span>Copyright &copy; Kucing Marvels</span>
-        </div>
-    </div>
-</footer>
--- bootstrap/php/koneksi.php
+++
@@ -1,5 +0,0 @@
-<?php
-$koneksi = mysqli_connect("localhost", "root", "", "peminjaman_ruang");
-if (mysqli_connect_errno()) {
-    die("Koneksi database gagal: " . mysqli_connect_error());
-}
